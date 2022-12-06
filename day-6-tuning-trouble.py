6f6ac102f35bf920b38d2c5773f0c0a2e230b346
codiak <codypumper@gmail.com>
2022-12-06 07:23:00 -0800

    Day 6: Tuning Trouble II

--- day-6-tuning-trouble.py
+++ day-6-tuning-trouble.py
@@ -4,20 +4,25 @@ from string import ascii_lowercase
 page = fetchInput("https://adventofcode.com/2022/day/6/input")
 text_data = page.text
 # text_data = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"  # test set
-letter_use = [0]*26  # assumes a-z character set
-marker = ''
-chars_proc = 0
 
-for i, char in enumerate(text_data):
-    ltr_i = ascii_lowercase.index(char)
-    letter_use[ltr_i] += 1
-    if i > 3:
-        head_i = ascii_lowercase.index(text_data[i-4])
-        letter_use[head_i] -= 1
-    # check if any 4 letters used once
-    if letter_use.count(1) == 4:
-        marker = text_data[i-3:i+1]
-        chars_proc = i+1
-        break
 
+def getMarker(text_data, n):
+    print(f'Processing text for {n} char distinct marker...')
+    letter_use = [0]*26  # assumes a-z character set
+    marker = ''
+    chars_proc = 0
+    for i, char in enumerate(text_data):
+        ltr_i = ascii_lowercase.index(char)
+        letter_use[ltr_i] += 1
+        if i > (n-1):
+            head_i = ascii_lowercase.index(text_data[i-n])
+            letter_use[head_i] -= 1
+        # check if any 4 letters used once
+        if letter_use.count(1) == n:
+            marker = text_data[i-(n-1):i+1]
+            chars_proc = i+1
+            break
+    return (marker, chars_proc)
+
+(marker, chars_proc) = getMarker(text_data, 14)
 print(f'First marker: {marker}, {chars_proc}')
