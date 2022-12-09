1cbb0699700ca04342b5eae3b787b06e049d6e87
codiak <codypumper@gmail.com>
2022-12-09 06:54:06 -0800

    Day 8: A little DRYer

--- day-8-tree-house.py
+++ day-8-tree-house.py
@@ -5,7 +5,6 @@ import numpy as np
 
 page = fetchInput("https://adventofcode.com/2022/day/8/input")
 text_data = page.text
-
 # Test Data
 # text_data = """30373111
 # 25512151
@@ -27,6 +26,17 @@ for x, row in enumerate(text_rows):
         data_arr[x, y] = int(char)
 
 print(f'Processing data with shape {data_arr.shape}...')
+
+
+def scoreDirection(tree, vector):
+    view = 0
+    for t in vector:
+        view += 1
+        if t >= tree:
+            break
+    return view
+
+
 # iterate over array once
 for y, row in enumerate(data_arr):
     for x, tree in enumerate(row):
@@ -35,7 +45,6 @@ for y, row in enumerate(data_arr):
         # Skip edges
         if 0 in [x, y] or y == maxYi or x == maxXi:
             visCount += 1
-            # view score defaults to zero
             continue
         # Set Up max
         if y == 1:
@@ -43,40 +52,28 @@ for y, row in enumerate(data_arr):
             viewU = 1
         else:
             maxU = max(data_arr[:, x][0:y])
-            for t in data_arr[:, x][0:y][::-1]:
-                viewU += 1
-                if t >= tree:
-                    break
+            viewU = scoreDirection(tree, data_arr[:, x][0:y][::-1])
         # Set Left max
         if x == 1:
             maxL = row[0]
             viewL = 1
         else:
             maxL = max(row[0:x])
-            for t in row[0:x][::-1]:
-                viewL += 1
-                if t >= tree:
-                    break
+            viewL = scoreDirection(tree, row[0:x][::-1])
         # Set Down max
         if y+1 == maxYi:
             maxD = data_arr[:, x][y+1]
             viewD = 1
         else:
             maxD = max(data_arr[:, x][y+1:maxYi+1])
-            for t in data_arr[:, x][y+1:maxYi+1]:
-                viewD += 1
-                if t >= tree:
-                    break
+            viewD = scoreDirection(tree, data_arr[:, x][y+1:maxYi+1])
         # Set Right max
         if x+1 == maxXi:
             maxR = row[x+1]
             viewR = 1
         else:
             maxR = max(row[x+1:maxXi+1])
-            for t in row[x+1:maxXi+1]:
-                viewR += 1
-                if t >= tree:
-                    break
+            viewR = scoreDirection(tree, row[x+1:maxXi+1])
         visLR = tree > maxL or tree > maxR
         visUD = tree > maxU or tree > maxD
         if visLR or visUD:
