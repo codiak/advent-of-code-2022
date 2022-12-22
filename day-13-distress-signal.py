18aa318b87b80c0a3ba3627c31c7a9aac61b91eb
codiak <codypumper@gmail.com>
2022-12-22 11:14:09 -0600

    Day 13: Distress Signal II

--- day-13-distress-signal.py
+++ day-13-distress-signal.py
@@ -1,34 +1,9 @@
 from utils import fetchInput
 import re
+from functools import cmp_to_key
 
 page = fetchInput("https://adventofcode.com/2022/day/13/input")
 text_data = page.text
-
-# text_data = """
-# [1,1,3,1,1]
-# [1,1,5,1,1]
-
-# [[1],[2,3,4]]
-# [[1],4]
-
-# [9]
-# [[8,7,6]]
-
-# [[4,4],4,4]
-# [[4,4],4,4,4]
-
-# [7,7,7,7]
-# [7,7,7]
-
-# []
-# [3]
-
-# [[[]]]
-# [[]]
-
-# [1,[2,[3,[4,[5,6,7]]]],8,9]
-# [1,[2,[3,[4,[5,6,0]]]],8,9]
-# """
 
 text_lines = list(re.split('\n\n|\n', text_data.strip()))
 packets = list(map(eval, text_lines))
@@ -42,41 +17,51 @@ def checkPacketsOrder(l, r):
         r = [r]
     if len(l) == 0 and len(r) > 0:
         # left ran out by being empty
-        return True
+        return 1
 
     for i, l_item in enumerate(l):
-        r_item = None
+        r_item = 0
         if i == len(r):
             # right ran out
-            return False
+            return -1
         else:
             r_item = r[i]
 
         if isinstance(l_item, int) and isinstance(r_item, int):
             if l_item != r_item:
                 # if integer, L < R
-                return l_item < r_item
+                if l_item < r_item:
+                    return 1
+                else:
+                    return -1
         else:
             inner_check = checkPacketsOrder(l_item, r_item)
-            if inner_check != None:
+            if inner_check in [1,-1]:
                 return inner_check
 
         if i+1 == len(l) and i+1 < len(r):
             # left going to run out
-            return True
-    return None
+            return 1
+    return 0
 
 
+# Part I
 ordered_i_sum = 0
 for pair_i in range(0, len(packets)-1, 2):
     left = packets[pair_i]
     right = packets[pair_i+1]
     compare = checkPacketsOrder(left, right)
     packet_i = int((pair_i/2)+1)
-    if compare == True:
+    if compare == 1:
         print(f'{packet_i} is ORDERED')
         ordered_i_sum += packet_i
     else:
         print(f'{packet_i} is NOT ORDERED')
-
 print(f'Indices sum: {ordered_i_sum}')
+
+# Part II
+packets.extend([[[2]],[[6]]])  # divider packets
+sorted_packets = sorted(packets, key=cmp_to_key(checkPacketsOrder), reverse=True)
+divOnei = sorted_packets.index([[2]])+1
+divTwoi = sorted_packets.index([[6]])+1
+print(f'Decoder key: {divOnei * divTwoi}')
